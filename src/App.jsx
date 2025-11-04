2dbc2b94fc4b552f6d5554bc105f4a2f57bb6ad0
Nik Nordquist <nnordquist3@gmail.com>
2025-11-04 07:53:01 -0600

    Created placeholder home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Ranker.io</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p>Want to rank all starting NFL quarterbacks? Have a definitive ranking of every Beatles album? 
+          <br></br>Use <strong>Ranker.io</strong> to create and share your own rankings!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
