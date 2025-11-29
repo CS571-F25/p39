3814ac0ffcfb6976d9d87b1f0e865ceb8372b533
Nik Nordquist <nnordquist3@gmail.com>
2025-11-29 14:53:34 -0600

    npm run build push 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import NavBar from './components/NavBar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import Tutorial from './screens/Tutorial'
-import Datasets from './screens/Datasets'
 import RankerGame from './screens/RankerGame'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path="*" element={<Home />} />
           <Route path="/tutorial" element={<Tutorial />} />
-          <Route path="/datasets" element={<Datasets />} />
           <Route path="/ranker" element={<RankerGame />} />
         </Routes>
       </div>
